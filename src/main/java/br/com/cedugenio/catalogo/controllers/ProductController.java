8fc3c66a9ed6174b6b754ee274d1a847b2832b3e
Carlos Eduardo Eugênio <tec_ceugenio@uolinc.com>
2021-02-06 09:59:21 -0300

    Adding Read,Update,Delete in Crud Product and other changes

--- src/main/java/br/com/cedugenio/catalogo/controllers/ProductController.java
+++ src/main/java/br/com/cedugenio/catalogo/controllers/ProductController.java
@@ -3,22 +3,52 @@ package br.com.cedugenio.catalogo.controllers;
 import br.com.cedugenio.catalogo.model.entities.Product;
 import br.com.cedugenio.catalogo.model.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path="/api/products")
 public class ProductController {
 
-    //Injeção de dependência
+    //Injecao de dependencia
     @Autowired
     private ProductRepository productRepository;
 
-
-    @PostMapping
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
      public @ResponseBody Product newProduct(Product product) {
         productRepository.save(product);
         return product;
+    }
+    @GetMapping
+    public Iterable<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping(path = "/name/{partName}")
+    public Iterable<Product> getProductsPerName
+            (@PathVariable String partName) {
+        return productRepository.findByNameContainingIgnoreCase(partName);
+    }
+
+
+    @GetMapping(path = "/page/{pageNumber}/{quantityPages}")
+    public Iterable<Product> getProductsPage
+            (@PathVariable int pageNumber , @PathVariable int quantityPages) {
+        if(quantityPages >= 5) quantityPages = 5;
+        Pageable page = PageRequest.of(pageNumber, quantityPages);
+        return productRepository.findAll(page);
+    }
+
+    @GetMapping(path="/{id}")
+    public Optional<Product> getProductWithId(@PathVariable int id) {
+        return productRepository.findById(id);
+    }
+
+    @DeleteMapping(path="/{id}")
+    public void deleteProduct(@PathVariable int id) {
+        productRepository.deleteById(id);
     }
 }
--- src/main/java/br/com/cedugenio/catalogo/model/repositories/ProductRepository.java
+++ src/main/java/br/com/cedugenio/catalogo/model/repositories/ProductRepository.java
@@ -1,9 +1,16 @@
 package br.com.cedugenio.catalogo.model.repositories;
 
 import br.com.cedugenio.catalogo.model.entities.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends
-        CrudRepository<Product, Integer> {
+        PagingAndSortingRepository<Product, Integer> {
+
+    public Iterable<Product> findByNameContainingIgnoreCase(String partName);
+
+    @Query("SELECT p FROM Product p WHERE p.nome LIKE %:name%")
+    public Iterable<Product>searchByNameLike(@Param("name") String name);
 
 }
